9a2a569f8c3f60d748f943b777e8b1340dd10626
MSNoyce <MSNoyce1@gmail.com>
2020-05-28 09:08:19 -0500

    Added Servlets

---
+++ servlet-job/src/main/java/group4/SpecificServlet.java
@@ -0,0 +1,60 @@
+package sparkproj;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.*;
+
+
+
+@WebServlet("/SpecificServlet")
+public class SpecificServlet extends HttpServlet {
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		  resp.getWriter().println("Hello!");
+		  String col = req.getParameter("DCounty");
+		  
+		  System.out.println("Get Request = " + col);
+		  resp.getWriter().println("Drought var: " + col);
+  
+				  //group wind from lowest to highest based on browser request
+		  //************************************************ */
+		  //Next Steps
+		  /*
+			  Call bash script to connect to EMR/Bucket with argument 
+				  to start this operation script. Also Pass in the arguments col ie.
+				  the county 
+			  Get results back from bucket
+			  return results to writer
+		  /*
+		  //-----------Database operations for Later----------//
+		  Dataset<Row> vars = ds.groupBy(col).count().orderBy(col("count"));
+		  String result = vars.showString(rnum, 10, false);
+		  /************************************************** */
+  
+		  String result = "No Database Found";
+		  resp.getWriter().println(result);
+
+
+
+	}
+
+
+
+}
